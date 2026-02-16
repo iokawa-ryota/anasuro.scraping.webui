79e5a7d47bdd3554c770e3de3d3c66d7bda2b254
iokwryt <iokwryt@gmail.com>
2026-02-17 01:27:23 +0900

    Auto-open Chrome after Flask startup

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
 cd /d %~dp0
-python _internal\app\app.py
+set "APP_URL=http://127.0.0.1:5000"
+
+start "Flask Server" cmd /k "cd /d %~dp0 && python _internal\app\app.py"
+timeout /t 2 /nobreak >nul
+start "" chrome "%APP_URL%"
